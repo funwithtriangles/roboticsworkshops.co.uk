5023d96ce6fb9b7a5eb9f0540160b22d1beac43e
Alex Kempton <alex@funwithtriangles.net>
2025-02-28 12:00:01 +0100

    testimonials slider

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,8 @@ import ejsPlugin from "@11ty/eleventy-plugin-ejs";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(ejsPlugin);
-  eleventyConfig.addPassthroughCopy("./src/assets/img");
+  eleventyConfig.addPassthroughCopy("./src/assets/**/*");
   eleventyConfig.addPassthroughCopy("./src/media");
-  eleventyConfig.addPassthroughCopy("./src/assets/app.css");
   eleventyConfig.addPassthroughCopy("./src/google06b7d7a15066b364.html");
 
   return {
